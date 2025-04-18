901c531e3912f28cc4fe1ae34e547b62004558e5
Ivan Bondar <ivanbondar.ivbo@gmail.com>
2025-04-18 19:46:53 +0300

    Add source parsing time calculation

--- src/main/java/com/crawler/web_crawler/service/implementation/NewsParserServiceImpl.java
+++ src/main/java/com/crawler/web_crawler/service/implementation/NewsParserServiceImpl.java
@@ -2,7 +2,6 @@ package com.crawler.web_crawler.service.implementation;
 
 import com.crawler.web_crawler.model.entity.NewsArticle;
 import com.crawler.web_crawler.model.entity.Source;
-import com.crawler.web_crawler.parser.JsoupParser;
 import com.crawler.web_crawler.parser.Parser;
 import com.crawler.web_crawler.repository.NewsArticleRepository;
 import com.crawler.web_crawler.service.NewsParserService;
@@ -12,14 +11,18 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeoutException;
 
 @Slf4j
 @Service
 public class NewsParserServiceImpl implements NewsParserService {
+    private static final int PARSE_TIME_LIMIT = 30;
+
     private final Parser parser;
     private final NewsArticleRepository repository;
 
     //TODO Consider adding a cache for article hashes, do not have to query the database for hashes every scan time
+    //TODO Update tests for this class
 
     private NewsParserServiceImpl(Parser parser, NewsArticleRepository repository) {
         this.parser = parser;
@@ -28,23 +31,39 @@ public class NewsParserServiceImpl implements NewsParserService {
 
     @Override
     public void parseAndSave(Source source) {
-        List<NewsArticle> articleList = parser.parse(source);
-        Set<String> hashes = getHashesFromDb(source);
-
-        articleList.removeIf((article) -> hashes.contains(article.getHash()));
-
-        if(articleList.isEmpty()) {
-            log.info("No new articles were found for source: {}", source.getUrl());
-            return;
-        }
-
         try {
-            log.info("Found and save {} new articles for service {}", articleList.size(), source.getUrl());
-            repository.saveAll(articleList);
-        } catch (DataIntegrityViolationException e) {
+            List<NewsArticle> uniqueNewsArticles = parseSource(source);
+
+            if(uniqueNewsArticles.isEmpty()) {
+                log.info("No new articles were found for source: {}", source.getUrl());
+                return;
+            }
+
+            log.info("Found and save {} new articles for service {}", uniqueNewsArticles.size(), source.getUrl());
+            repository.saveAll(uniqueNewsArticles);
+        } catch (DataIntegrityViolationException | TimeoutException e) {
             log.error("The list of articles was not saved.\nReason: {}", e.getMessage());
             throw new RuntimeException("Runtime exception while saving data: " + e.getMessage(), e);
         }
+    }
+
+    private List<NewsArticle> parseSource(Source source) throws TimeoutException {
+        long startParseTime = System.currentTimeMillis();
+
+        List<NewsArticle> articleList = parser.parse(source);
+
+        long duration = (System.currentTimeMillis() - startParseTime) / 1000;
+        log.info("Scanning {} took {} seconds", source.getUrl(), duration);
+
+        if(duration > PARSE_TIME_LIMIT) {
+            log.error("Parsing source {} exceeded time limit ({} sec)", source.getUrl(), PARSE_TIME_LIMIT);
+            throw new TimeoutException("Parsing source: " + source.getUrl() + " exceeded time limit (" + PARSE_TIME_LIMIT + " sec)");
+        }
+
+        Set<String> hashes = getHashesFromDb(source);
+        articleList.removeIf((article) -> hashes.contains(article.getHash()));
+
+        return articleList;
     }
 
     private Set<String> getHashesFromDb(Source source) {
